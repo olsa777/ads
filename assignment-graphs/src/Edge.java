666e0c173085116e0e915622bd6145e7fe0610c4
olsa777 <125235597+olsa777@users.noreply.github.com>
2026-05-23 12:01:52 +0500

    Add weight attribute to Edge class

--- assignment-graphs/src/Edge.java
+++ assignment-graphs/src/Edge.java
@@ -1,18 +1,29 @@
 public class Edge {
+
     private Vertex source;
     private Vertex destination;
-    public Edge(Vertex source, Vertex destination) {
+    private int weight;
+
+    public Edge(Vertex source, Vertex destination, int weight) {
         this.source = source;
         this.destination = destination;
+        this.weight = weight;
     }
+
     public Vertex getSource() {
         return source;
     }
+
     public Vertex getDestination() {
         return destination;
     }
+
+    public int getWeight() {
+        return weight;
+    }
+
     @Override
     public String toString() {
-        return source + " -> " + destination;
+        return source + " -> " + destination + " (weight: " + weight + ")";
     }
 }
